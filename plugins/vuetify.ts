5127a1114ea74554158eeed4cb1a0c88a51c270d
Akayy <techakayy@gmail.com>
2024-09-14 21:07:57 +1000

    vuetify  plugin updated

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,10 +1,8 @@
 import 'vuetify/styles'
 import {createVuetify} from 'vuetify'
-import * as components from 'vuetify/components'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    components,
     ssr: true,
   })
   app.vueApp.use(vuetify)
